2817fb3164665c5904bf5bb6ecd930896a112fa9
xianengqi <xianengqi@gmail.com>
2020-11-13 02:22:28 +0800

    Initial Commit

---
+++ SampleCode+CoreDate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleCode+CoreDate
+//
+//  Created by 夏能啟 on 2020/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
